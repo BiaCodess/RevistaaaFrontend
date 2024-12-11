134cbc4071d990a231dd880363bc4495fd125296
BiaCodess <pereiracoelhobeatriz@gmail.com>
2024-12-11 17:21:51 -0300

    Update Loginoficial.jsx

--- src/components/Loginoficial.jsx
+++ src/components/Loginoficial.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from "./Loginoficial.module.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     const [email, setEmail] = useState('');
